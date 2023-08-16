88db92ff9406b319815832cb68f07df59b0586a6
José Fernández-Caminero <fdezcaminero@gmail.com>
2023-08-16 19:20:34 -0400

    Fix linters

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Inputfunction = () => (
-<div className="minus1margin">
-  <input className="inputStyle" placeholder="0"></input>
-</div>
+  <div className="minus1margin">
+    <input className="inputStyle" placeholder="0" />
+  </div>
 );
 
 export default Inputfunction;
