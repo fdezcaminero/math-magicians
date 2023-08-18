ec67a7312a4d3ecd29e2ccffc8e134c3061280c0
José Fernández-Caminero <fdezcaminero@gmail.com>
2023-08-18 10:42:31 -0400

    Remove useless code

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 const Inputfunction = ({ inputvalue }) => (
   <div className="minus1margin inputStyle">
     {inputvalue}
   </div>
 );
-
-Inputfunction.propTypes = {
-  inputvalue: PropTypes.string.isRequired,
-};
-
-Inputfunction.defaultProps = {
-  inputvalue: 0,
-};
 
 export default Inputfunction;
