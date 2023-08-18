bebad1cf91cf7bf7dab676eb79f2c812660d6a85
José Fernández-Caminero <fdezcaminero@gmail.com>
2023-08-18 11:00:50 -0400

    Add propTypes

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Inputfunction = ({ inputvalue }) => (
   <div className="minus1margin inputStyle">
     {inputvalue}
   </div>
 );
+
+Inputfunction.propTypes = {
+  inputvalue: PropTypes.string.isRequired,
+};
 
 export default Inputfunction;
