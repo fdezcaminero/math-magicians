3a5709d488a962bc403c072bd9e2022810488a31
José Fernández-Caminero <fdezcaminero@gmail.com>
2023-08-18 10:37:51 -0400

    Implement logic

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Inputfunction = () => (
-  <div className="minus1margin">
-    <input className="inputStyle" placeholder="0" />
+const Inputfunction = ({ inputvalue }) => (
+  <div className="minus1margin inputStyle">
+    {inputvalue}
   </div>
 );
+
+Inputfunction.propTypes = {
+  inputvalue: PropTypes.string.isRequired,
+};
+
+Inputfunction.defaultProps = {
+  inputvalue: 0,
+};
 
 export default Inputfunction;
