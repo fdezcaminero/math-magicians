b6fcf2dd72e37af7b8908d0e67c01ad1d5ffcd3f
José Fernández-Caminero <fdezcaminero@gmail.com>
2023-08-19 11:31:33 -0400

    Add loading state and error state

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 
 const Quotes = () => {
-  const [quote, setQuote] = useState('Quotes on the way');
-  const [loading, setLoading] = useState('Quotes on the way');
-  const [anError, setAnError] = useState('Quotes on the way');
+  const [quote, setQuote] = useState();
+  const [loading, setLoading] = useState(true);
+  const [anError, setAnError] = useState();
 
   useEffect(() => {
     fetch('https://api.api-ninjas.com/v1/quotes?category=happiness', {
@@ -15,18 +15,31 @@ const Quotes = () => {
       .then((data) => {
         const fetchedQuote = data[0] || 'No quotes available';
         setQuote(fetchedQuote);
+        setLoading(false);
       })
       .catch((error) => {
-        console.error('Error: ', error);
+        console.log(error);
+        setLoading(false);
+        setAnError(true);
       });
   }, []);
 
-  return (
-    <div className="borderStyles">
-      <p>{quote.quote}</p>
-      <p>{quote.author}</p>
-    </div>
-  );
+  let content;
+
+  if (loading) {
+    content = <p>Loading...</p>;
+  } else if (anError) {
+    content = <p>An error occurred while fetching data.</p>;
+  } else {
+    content = (
+      <>
+        <p>{quote.quote}</p>
+        <p>{quote.author}</p>
+      </>
+    );
+  }
+
+  return <div className="borderStyles">{content}</div>;
 };
 
 export default Quotes;
