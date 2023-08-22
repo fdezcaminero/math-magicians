eb515492c08d8401e6d01f0246b7d09afd392e15
José Fernández-Caminero <fdezcaminero@gmail.com>
2023-08-22 19:12:33 -0400

    Fix SPA

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
-import React from 'react';
-import './App.css';
+// App.js
+import {
+  BrowserRouter, Routes, Route, Outlet,
+} from 'react-router-dom';
+import Home from './components/Home';
 import Calculator from './components/Calculator';
 import Quotes from './components/Quotes';
+import './App.css';
+
+function Layout() {
+  return (
+    <>
+      <h1>Math magicians</h1>
+      <Outlet />
+    </>
+  );
+}
 
 function App() {
   return (
-    <div>
-      <Calculator />
-      <Quotes />
-    </div>
+    <section>
+      <Home />
+      <Layout />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<div>Home page</div>} />
+            <Route path="/calculator" element={<Calculator />} />
+            <Route path="/quotes" element={<Quotes />} />
+            <Route path="*" element={<div>If page not found it goes here</div>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </section>
   );
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <section>
+      <h1>The math magicians</h1>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Doloribus sequi obcaecati ullam cupiditate numquam nemo aliquid ipsa id placeat?
+        Veritatis similique impedit voluptate cupiditate fugit unde consequatur.
+        Perspiciatis, quae sint.
+      </p>
+    </section>
+  );
+}
+
+export default Home;
